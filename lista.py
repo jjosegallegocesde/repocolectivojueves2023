310946d4601d5b77918cfa460ae6fd086c5df0f4
chenaoh <mfvilladaqu@gmail.com>
2023-03-02 08:43:43 -0500

    Nuevas tecnologias se crean listas

---
+++ lista.py
@@ -0,0 +1,9 @@
+#Lista de datos
+numeros =[1,2,3,4,5]
+#Acceder a todos los datos al mismo tiempo
+print(numeros)
+#Acceder a un elemento de la lista 
+print(numeros[0])
+#acceder de forma individual a cada elemento de la lista
+for numero in numeros:
+    print (numero)
